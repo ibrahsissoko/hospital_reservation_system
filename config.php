38764d61cc6fe4718654ca57b689b958418d058d
Luke Klinker <lklinker1@gmail.com>
2015-03-04 15:07:26 -0600

    try adding database support

--- config.php
+++ config.php
@@ -1,4 +1,16 @@
 <?php 
-	// this is where we can store some of the database information
-	// so that it persists across different pages
+	define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+	define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+	define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+	define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+	define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+
+	$dbhost = constant("DB_HOST"); // Host name 
+	$dbport = constant("DB_PORT"); // Host port
+	$dbusername = constant("DB_USER"); // Mysql username 
+	$dbpassword = constant("DB_PASS"); // Mysql password 
+	$db_name = constant("DB_NAME"); // Database name 
+
+	$mysqlCon = mysqli_connect($dbhost, $dbusername, $dbpassword, "", $dbport) or die("Error: " . mysqli_error($mysqlCon));
+	mysqli_select_db($mysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon));
 ?>
