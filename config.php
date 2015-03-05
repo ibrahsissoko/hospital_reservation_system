e5b9e4e605c1585804bc2924953e999f7592d4df
Luke Klinker <lklinker1@gmail.com>
2015-03-05 09:26:37 -0600

    try using pdo

--- config.php
+++ config.php
@@ -11,5 +11,18 @@
 	$dbpassword = constant("DB_PASS"); // Mysql password 
 	$db_name = "wal";//"constant("DB_NAME")"; // Database name 
 
-	$conn = mysqli_connect($dbhost, $dbusername, $dbpassword, $db_name, $dbport) or die("Error: no connection");
+	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
+
+    try { 
+    	$db = new PDO("mysql:host={$host};port={$port};dbname={$dbname};charset=utf8", $username, $password, $options); 
+    } catch(PDOException $ex) { 
+    	die("Failed to connect to the database: " . $ex->getMessage());
+    } 
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
+
+    header('Content-Type: text/html; charset=utf-8'); 
+
+    session_start(); 
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     $message = "";
 
     require("config.php");
+    
     if(!empty($_POST)) { 
       $username = $_POST['username'];
       $password = $_POST['password'];
