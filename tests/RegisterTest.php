6807f3ff608bd7c1c26802b954a1c3c5e95bd743
Will Tollefson <william-tollefson@uiowa.edu>
2015-03-11 11:29:18 -0500

    Fix

--- tests/RegisterTest.php
+++ tests/RegisterTest.php
@@ -3,7 +3,7 @@
 class RegisterTest  extends PHPUnit_Framework_TestCase {
 
     public function testInitialize() {
-        $r = new Registration();
+        $r = new Register();
         $r->initializeValues();
 
         $string = $r->getNoEmail();
