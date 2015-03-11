e80ac6e73ba9611dc843d54c2a4949edc9d65baf
Luke Klinker <lklinker1@gmail.com>
2015-03-11 15:13:26 -0500

    try autoload again

--- src/config.php
+++ src/config.php
@@ -1,6 +1,8 @@
 <?php
-    set_include_path ( "./classes" );
-    spl_autoload_register ();
+    set_include_path("./classes");
+    spl_autoload_register(function ($class) {
+        include 'classes/' . $class . '.php';
+    });
 
 	define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 	define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
--- src/register.php
+++ src/register.php
@@ -126,11 +126,7 @@
                 // Redirect to login.
                 header("Location: index.php");
                 die("Redirecting to index.php");
-            } else {
-                die("email point");
             }
-        } else {
-            die("no email: " . $noEmail . " inco email: " . $incorrectEmail . " noPass: " . $noPassword . " no confirmPass: " . $noConfirmPassword . " no pass match" . $noPasswordMatch . " no access code: " . $noAccessCode);
         }
     } 
 ?>
