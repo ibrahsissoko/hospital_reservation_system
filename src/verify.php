c4fa6c60d3c1bdeb47bcfa2c31e50815d81ac8e8
Will Tollefson <william-tollefson@uiowa.edu>
2015-03-11 12:01:43 -0500

    Created container verify.php file.

---
+++ src/verify.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Hospital Management</title>
+    <meta name="description" content="Hospital management system for Intro to Software Engineering">
+    <meta name="author" content="WAL Consulting">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script src="assets/bootstrap.min.js"></script>
+    <link href="assets/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="assets/styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+
+<div class="navbar navbar-fixed-top navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a href="src/home.php" class="brand">Hospital Management</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav pull-right">
+          <li><a href="src/register.php">Register</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container hero-unit">
+    <h1>Verification Page</h1> <br />
+</div>
+
+</body>
+</html>
