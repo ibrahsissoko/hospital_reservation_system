6d02e3e2e3770b681c656e6322ad291c2fea0e1f
Will Tollefson <william-tollefson@uiowa.edu>
2015-03-12 22:42:00 -0500

    Moved some files to home dir for now.

--- src/register.php
+++ src/register.php
@@ -4,7 +4,7 @@
     echo "2";
     require("config.php");
     echo "3";
-    require_once('dirname(__FILE__)' . '/../PHPMailer/PHPMailerAutoload.php');
+    require("PHPMailerAutoload.php");
     echo "4";
     // Initialize error messages to blank.
     $r = new Register();
