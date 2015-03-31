14cc5d2461458bd4e5a8379a8f6b6c2626966c5d
Will Tollefson <william-tollefson@uiowa.edu>
2015-03-30 21:12:21 -0500

    Try again.

---
+++ src/cancel_appointment.php
@@ -0,0 +1,74 @@
+<?php
+
+    include_once('../AutoLoader.php');
+    AutoLoader::registerDirectory('../src/classes');
+
+    require("config.php");
+    require("MailFiles/PHPMailerAutoload.php");
+    
+    if(empty($_SESSION['user'])) {
+        header("Location: ../index.php");
+        die("Redirecting to index.php");
+    } else if (!empty($_GET)) {
+        
+        $query = "
+            DELETE *
+            FROM users
+            WHERE
+              id = :id
+        ";
+        $query_params = array(
+            ':id' => $_GET['id']
+        );
+        
+        try {
+            $stmt = $db->prepare($query);
+            $result = $stmt->execute($query_params);
+        } catch(PDOException $ex) {
+                die("Failed to run query: " . $ex->getMessage());
+        }
+        
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <style>.error {color: #FF0000;}</style>
+    <style>.success {color: #00FF00;</style>
+    <meta charset="utf-8">
+    <title>Hospital Management</title>
+    <meta name="description" content="Hospital management system for Intro to Software Engineering">
+    <meta name="author" content="WAL Consulting">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script src="../assets/bootstrap.min.js"></script>
+    <link href="../assets/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="../assets/styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<div class="navbar navbar-fixed-top navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a href="home.php" class="brand">Hospital Management</a>
+      <div class="nav-collapse">
+        <ul class="nav pull-right">
+          <li><a href="home.php">Home</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+ 
+<div class="container hero-unit">
+    <h1>Appointment Deleted</h1>
+</div>
+
+</body>
+</html>
