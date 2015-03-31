5745f0025b599085b400ed93de03e9978e67ec6b
Will Tollefson <william-tollefson@uiowa.edu>
2015-03-30 20:50:45 -0500

    Fixed which user id is being called in links while viewing appointments.

--- src/view_appointments.php
+++ src/view_appointments.php
@@ -74,7 +74,29 @@
             die("Failed to run query: " . $ex->getMessage());
         }
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $link = "http://wal-engproject.rhcloud.com/src/user_page.php?id=" . $row['id'];
+            // Need to query the users table to get the user ID we are looking for
+            // with the link to the user profile page.
+            $query = "
+                   SELCECT *
+                   FROM users
+                   WHERE
+                       first_name = :appointmentWithFirstName
+                       AND
+                       last_name = :appointmentWithLastName
+                    ";
+            $name = explode(" ", $row[$appointmentWith . "_name"]);
+            $query_params = array(
+                ":appointmentWithFirstName" => $name[0],
+                ":appointmentWithLastName" => $name[1]
+            );
+            try {
+                $stmt = $db->prepare($query);
+                $result = $stmt->execute($query_params);
+            } catch(PDOException $ex) {
+                die("Failed to run query: " . $ex->getMessage());
+            }
+            $entry = $stmt->fetch(PDO::FETCH_ASSOC);
+            $link = "http://wal-engproject.rhcloud.com/src/user_page.php?id=" . $entry['id'];
             echo "<p>You have an appointment with <a href=\"" . $link . "\">"
             . $row[$appointmentWith . "_name"] . "</a> on " . $row["date"] . " at " . $row["time"] . "</p>";
         }
