3beb2ac493164bd2a2d658557678691dbac6a510
Will Tollefson <william-tollefson@uiowa.edu>
2015-04-01 12:00:16 -0500

    Debug statements.

--- src/schedule_appointment.php
+++ src/schedule_appointment.php
@@ -180,7 +180,7 @@
                     $endTime += 24;
                 }
                 // Create an empty value.
-                echo "<option value=\"\" selected=\"selected\">$beginTime, $endTime, " . $beginTime . " " . $endTime . "</option>";
+                echo "<option value=\"\" selected=\"selected\">" . $shift['start_time'] . ", " . $shift['end_time'] . "</option>";
                 for($i = $beginTime; $i < $endTime; $i++) {
                     if ($i < 12) {
                         echo "<option value =\"" . $i . ":00 am\">" . $i . ":00 am</option>";
