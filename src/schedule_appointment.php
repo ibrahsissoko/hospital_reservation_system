5baf7b708615af9b04a8a59d1b040db1ebc91978
Will Tollefson <william-tollefson@uiowa.edu>
2015-04-01 11:38:20 -0500

    Debug statements.

--- src/schedule_appointment.php
+++ src/schedule_appointment.php
@@ -120,6 +120,7 @@
                             echo "<option value=\"" . $row["first_name"] . " " . $row["last_name"] 
                                     . " " . $row["degree"] . "\">" . $row["first_name"] . " " 
                                     . $row["last_name"] . " " . $row["degree"] . "</option>";
+                            echo "option value=\"1\">1</option>";
                     }
                 } else {
                     // Create a blank entry.
@@ -132,6 +133,7 @@
                                     . " " . $row["degree"] . "\" selected=\"selected\">" 
                                     . $row["first_name"] . " " . $row["last_name"] . " " 
                                     . $row["degree"] . "</option>";
+                            echo "option value=\"2\">1</option>";
                         } else if ($docName == $row["first_name"] . " " . $row["last_name"]) {
                             echo "<option value=\"" . $row["first_name"] . " " . $row["last_name"]
                                     . " " . $row["degree"] . "\" selected=\"selected\">" 
@@ -139,10 +141,12 @@
                                     . $row["degree"] . "</option>";
                             // Set the post value of the doctor's name.
                             $_POST['doctor_name'] = $docName . " " . $row['degree'];
+                            echo "option value=\"3\">1</option>";
                         } else {
                             echo "<option value=\"" . $row["first_name"] . " " . $row["last_name"] 
                                     . " " . $row["degree"] . "\">" . $row["first_name"] . " " 
                                     . $row["last_name"] . " " . $row["degree"] . "</option>";
+                            echo "option value=\"4\">1</option>";
                         }
                     }
                 }
