bb1e8be78846758aae6739542c456320219e573e
Will Tollefson <william-tollefson@uiowa.edu>
2015-04-01 18:06:37 -0500

    Try again.

--- src/schedule_appointment.php
+++ src/schedule_appointment.php
@@ -99,7 +99,7 @@
     <h1>Schedule an Appointment</h1> <br />
     <form action="schedule_appointment.php" method="post" id="mainForm">
         Which Doctor Would You Like?<br/>
-        <select name="doctor_name" id="doctorNameUpdate">
+        <select name="doctor_name" onchange="doctorNameUpdated()">
             <?php
             if(!empty($_GET['id'])) {
                 $query = "
