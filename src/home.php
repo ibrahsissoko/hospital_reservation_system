7550ad52f8326fca29d94da0d8eab0f6554161a4
Luke Klinker <lklinker1@gmail.com>
2015-04-06 18:18:36 -0500

    another test

--- src/home.php
+++ src/home.php
@@ -90,7 +90,7 @@
 
         echo "<a href=\"view_appointments.php\">View Current Appointments Scheduled</a>";
     ?>
-    <br>User Type:      <?php echo $_SESSION['user']['department'];
+    <br>User Type:      <?php echo $_SESSION['user']['id'];
                             $query = "
                                     SELECT *
                                     FROM user_types
