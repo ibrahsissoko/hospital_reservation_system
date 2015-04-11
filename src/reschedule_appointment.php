6a6cefb470d8fcef334ae757345c1d4fec96a74e
Will Tollefson <william-tollefson@uiowa.edu>
2015-04-11 00:26:42 -0500

    Fix

--- src/reschedule_appointment.php
+++ src/reschedule_appointment.php
@@ -75,149 +75,146 @@
 <div class="container hero-unit">
     <h1>Reschedule an Appointment</h1> <br />
     <form action="" method="post" id="mainForm">
-            <?php
+        <?php
             echo "You are currently scheduled with " . $appointmentInfo['doctor_name'] . ".<br/>";
             echo "Date:<br/>";
-            echo '<input type="text" id="datepicker" name ="date" readonly="readonly" value="' . $date . '" onchange="dateUpdated()"/><br/>';
-            
-            if (!empty($_POST['date'])) {
-                $query2 = "
-                       SELECT *
-                       FROM users
-                       WHERE
-                           user_type_id = :id
-                           AND
-                           first_name = :doctorFirstName
-                           AND
-                           last_name = :doctorLastName
-                        ";
-                $name = explode(" ", $appointmentInfo['doctor_name']);
-                $query_params2 = array(
-                    ":id" => 2,
-                    ":doctorFirstName" => $name[0],
-                    ":doctorLastName" => $name[1]
-                );
-                try {
-                    $stmt2 = $db->prepare($query2);
-                    $result2 = $stmt2->execute($query_params2);
-                } catch(PDOException $ex) {
-                    die("Failed to run query: " . $ex->getMessage());
-                }
-                $docInfo = $stmt2->fetch();
-                $query = '
-                        SELECT *
-                        FROM shift
-                        WHERE
-                            id = :id
-                        ';
-                $query_params = array(
-                    ':id' => $docInfo['shift_id']
-                );
-                try {
-                    $stmt = $db->prepare($query);
-                    $result = $stmt->execute($query_params);
-                    $shift = $stmt->fetch();
-                } catch(PDOException $e) {
-                    die("Failed to run query: " . $e->getMessage());
-                }
-                $beginTime = intval($shift['start_time']);
-                $endTime = intval($shift['end_time']);
-                if ($endTime < $beginTime) {
-                    // E.g 19-3 => 19-27 for simplicity.
-                    $endTime += 24;
-                }
-                // Determine appointments that have already been scheduled.
-                $query = '
-                        SELECT *
-                        FROM appointment
-                        WHERE
-                            doctor_name = :doctorName
-                            AND
-                            date = :date
-                        ';
-                $query_params = array(
-                    ':doctorName' => $appointmentInfo['doctor_name'],
-                    ':date' => $_POST['date']
-                );
-                try {
-                    $stmt = $db->prepare($query);
-                    $result = $stmt->execute($query_params);
-                } catch(PDOException $e) {
-                    die("Failed to run query: " . $e->getMessage());
-                }
-                $preBookedTimes = array();
-                while ($alreadyBookedTimes = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    $times = explode(":", $alreadyBookedTimes["time"]);
-                    array_push($preBookedTimes, intval($times[0]));
-                }
-                // Check if all appointment times have been booked for this day.
-                if (sizeof($preBookedTimes) != 8) {
-                    echo "Other times available:<br/>";
-                    echo '<select name="time" id="time">';
-                    // Determine which days go in the select field.
-                    for($i = $beginTime; $i < $endTime; $i++) {
-                        $alreadyBooked = false;
-                        if ($i < 12) {
-                            foreach($preBookedTimes as $hour) {
-                                if($i == $hour) {
-                                    $alreadyBooked = true;
-                                    break;
-                                }
-                            }
-                            if (!$alreadyBooked) {
-                                echo "<option value =\"" . $i . ":00 am\">" . $i . ":00 am</option>";
-                            }
-                        } else if ($i == 12) {
-                            foreach($preBookedTimes as $hour) {
-                                if($i == $hour) {
-                                    $alreadyBooked = true;
-                                    break;
-                                }
-                            }
-                            if(!$alreadyBooked) {
-                                echo "<option value =\"" . $i . ":00 pm\">" . $i . ":00 pm</option>";
-                            }
-                        } else if ($i > 12 && $i < 24) {
-                            $val = $i - 12;
-                            foreach($preBookedTimes as $hour) {
-                                if($val == $hour) {
-                                    $alreadyBooked = true;
-                                    break;
-                                }
-                            }
-                            if(!$alreadyBooked) {
-                                echo "<option value =\"" . $val . ":00 pm\">" . $val . ":00 pm</option>";
-                            }
-                        } else if ($i == 24) {
-                            $val = $i - 12;
-                            foreach($preBookedTimes as $hour) {
-                                if($val == $hour) {
-                                    $alreadyBooked = true;
-                                    break;
-                                }
-                            }
-                            if(!$alreadyBooked) {
-                                echo "<option value =\"" . $val . ":00 am\">" . $val . ":00 am</option>";
-                            }
-                        } else {
-                            $val = $i - 24;
-                            foreach($preBookedTimes as $hour) {
-                                if($val == $hour) {
-                                    $alreadyBooked = true;
-                                    break;
-                                }
-                            }
-                            if(!$alreadyBooked) {
-                                echo "<option value =\"" . $val . ":00 am\">" . $val . ":00 am</option>";
+            echo '<input type="text" id="datepicker" name ="date" readonly="readonly" value="' . (empty($_POST['date'])) ? $date : $_POST['date'] . '" onchange="dateUpdated()"/><br/>';
+            $query2 = "
+                   SELECT *
+                   FROM users
+                   WHERE
+                       user_type_id = :id
+                       AND
+                       first_name = :doctorFirstName
+                       AND
+                       last_name = :doctorLastName
+                    ";
+            $name = explode(" ", $appointmentInfo['doctor_name']);
+            $query_params2 = array(
+                ":id" => 2,
+                ":doctorFirstName" => $name[0],
+                ":doctorLastName" => $name[1]
+            );
+            try {
+                $stmt2 = $db->prepare($query2);
+                $result2 = $stmt2->execute($query_params2);
+            } catch(PDOException $ex) {
+                die("Failed to run query: " . $ex->getMessage());
+            }
+            $docInfo = $stmt2->fetch();
+            $query = '
+                    SELECT *
+                    FROM shift
+                    WHERE
+                        id = :id
+                    ';
+            $query_params = array(
+                ':id' => $docInfo['shift_id']
+            );
+            try {
+                $stmt = $db->prepare($query);
+                $result = $stmt->execute($query_params);
+                $shift = $stmt->fetch();
+            } catch(PDOException $e) {
+                die("Failed to run query: " . $e->getMessage());
+            }
+            $beginTime = intval($shift['start_time']);
+            $endTime = intval($shift['end_time']);
+            if ($endTime < $beginTime) {
+                // E.g 19-3 => 19-27 for simplicity.
+                $endTime += 24;
+            }
+            // Determine appointments that have already been scheduled.
+            $query = '
+                    SELECT *
+                    FROM appointment
+                    WHERE
+                        doctor_name = :doctorName
+                        AND
+                        date = :date
+                    ';
+            $query_params = array(
+                ':doctorName' => $appointmentInfo['doctor_name'],
+                ':date' => (empty($_POST['date'])) ? $date : $_POST['date']
+            );
+            try {
+                $stmt = $db->prepare($query);
+                $result = $stmt->execute($query_params);
+            } catch(PDOException $e) {
+                die("Failed to run query: " . $e->getMessage());
+            }
+            $preBookedTimes = array();
+            while ($alreadyBookedTimes = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $times = explode(":", $alreadyBookedTimes["time"]);
+                array_push($preBookedTimes, intval($times[0]));
+            }
+            // Check if all appointment times have been booked for this day.
+            if (sizeof($preBookedTimes) != 8) {
+                echo "Other times available:<br/>";
+                echo '<select name="time" id="time">';
+                // Determine which days go in the select field.
+                for($i = $beginTime; $i < $endTime; $i++) {
+                    $alreadyBooked = false;
+                    if ($i < 12) {
+                        foreach($preBookedTimes as $hour) {
+                            if($i == $hour) {
+                                $alreadyBooked = true;
+                                break;
                             }
                         }
+                        if (!$alreadyBooked) {
+                            echo "<option value =\"" . $i . ":00 am\">" . $i . ":00 am</option>";
+                        }
+                    } else if ($i == 12) {
+                        foreach($preBookedTimes as $hour) {
+                            if($i == $hour) {
+                                $alreadyBooked = true;
+                                break;
+                            }
+                        }
+                        if(!$alreadyBooked) {
+                            echo "<option value =\"" . $i . ":00 pm\">" . $i . ":00 pm</option>";
+                        }
+                    } else if ($i > 12 && $i < 24) {
+                        $val = $i - 12;
+                        foreach($preBookedTimes as $hour) {
+                            if($val == $hour) {
+                                $alreadyBooked = true;
+                                break;
+                            }
+                        }
+                        if(!$alreadyBooked) {
+                            echo "<option value =\"" . $val . ":00 pm\">" . $val . ":00 pm</option>";
+                        }
+                    } else if ($i == 24) {
+                        $val = $i - 12;
+                        foreach($preBookedTimes as $hour) {
+                            if($val == $hour) {
+                                $alreadyBooked = true;
+                                break;
+                            }
+                        }
+                        if(!$alreadyBooked) {
+                            echo "<option value =\"" . $val . ":00 am\">" . $val . ":00 am</option>";
+                        }
+                    } else {
+                        $val = $i - 24;
+                        foreach($preBookedTimes as $hour) {
+                            if($val == $hour) {
+                                $alreadyBooked = true;
+                                break;
+                            }
+                        }
+                        if(!$alreadyBooked) {
+                            echo "<option value =\"" . $val . ":00 am\">" . $val . ":00 am</option>";
+                        }
                     }
-                    echo "</select><br/><br/>";
-                } else {
-                    echo '</br><p>No appointments available</p><br/><br/>';
                 }
-                echo '<input type="submit" name ="submitButton" class="btn btn-info" value="Update"/><br/><br/>';
+                echo "</select><br/><br/>";
+            } else {
+                echo '</br><p>No appointments available</p><br/><br/>';
             }
+            echo '<input type="submit" name ="submitButton" class="btn btn-info" value="Update"/><br/><br/>';
         ?>
         <script>
         function dateUpdated() {
