743ea1a28ed9a3356bbee55341d1ee1ad50c610d
Will Tollefson <william-tollefson@uiowa.edu>
2015-04-15 11:10:55 -0500

    Try again.

--- src/classes/Diagnosis.php
+++ src/classes/Diagnosis.php
@@ -6,6 +6,7 @@ class Diagnosis {
     public $patientEmail;
     private $doctorName;
     private $patientName;
+    private $drug_name;
     private $date;
     private $time;
     private $db;
@@ -58,6 +59,7 @@ class Diagnosis {
                 } catch(PDOException $e) {
                     die("Failed to gather prescription information. " . $e->getMessage());
                 }
+                $this->drug_name = $medication;
                 $row = $stmt->fetch();
                 $this->prescriptionID = $row['id'];
                 $this->amount_due += intval($row['price']);
@@ -240,29 +242,70 @@ class Diagnosis {
         $pdf = new FPDF();
         $pdf->AddPage();
         $pdf->SetFont('Arial','B',22);
-        $pdf->Cell($pdf->w,40,'Billing Information',0,1,'C');
+        $pdf->Cell($pdf->w-20,40,'Billing Receipt',0,1,'C');
         $pdf->SetFont('Arial','',12);
-        $pdf->MultiCell($pdf->w-20,10,'Thank you ' . $this->patientName . ' for scheduling and attending your appointment with ' . $this->doctorName
-                . '. The doctor had the following observations:',0,1);
-        $pdf->MultiCell(100,10,$this->observations,0,1);
+        
+        $query = '
+                SELECT *
+                FROM prescription
+                WHERE
+                    id = :id
+                  ';
+        $query_params = array(
+            ':id' => $this->prescriptionID
+        );
+        try {
+            $stmt = $db->prepare($query);
+            $stmt->execute($query_params);
+        } catch(PDOException $ex) {
+            die("Failed to run query: " . $ex->getMessage());
+        }
+        $prescriptionInfo = $stmt->fetch();
+
+        $pdf->MultiCell($pdf->w-20,10,'Thank you, ' . $this->patientName . ', for scheduling and attending your appointment with ' . $this->doctorName
+                . '. The doctor had the following observations:',0);
+        $pdf->SetFont('Arial','B');
+        $pdf->MultiCell($pdf->w-60,8,$this->observations,0,'C');
+        $pdf->SetFont('Arial','',12);
         $pdf->Write(10,'These observations led to the following diagnosis: ');
         $pdf->SetFont('Arial','B');
         $pdf->Cell(30,10,$this->diagnosis,0,1);
         $pdf->SetFont('Arial','');
-        $pdf->Write($pdf->w-20,10,'You have therefore been given this medication: ');
-        $pdf->SetFont('Arial','B');
-        $pdf->Cell(30,10,'SOME MEDICATION',0,1);
-        $pdf->SetFont('Arial','');
-        $pdf->MultiCell($pdf->w-20,10,'Please submit you payment soon by clicking on the Pay Bills link on the home page of your'
-                . ' account or by clicking HERE.',0,1);
+        if (!empty($prescriptionInfo)) {
+            $pdf->Write(10,'You have therefore been given this medication: ');
+            $pdf->SetFont('Arial','B');
+            $pdf->Cell(30,10,$this->medication,0,1);
+            $pdf->SetFont('Arial','');
+            $pdf->Write(10,'General information: ');
+            $pdf->SetFont('Arial','B');
+            $pdf->MultiCell(60,8,$prescriptionInfo['property'],0);
+            $pdf->SetFont('Arial','');
+            $pdf->Write(10,'Directions of usage: ');
+            $pdf->SetFont('Arial','B');
+            $pdf->MultiCell(60,8,$prescriptionInfo['usage_directions'],0);
+            $pdf->SetFont('Arial','');
+        }
+        $pdf->Write(10,'Please submit you payment soon by clicking on the Pay link next to your bill on the view bills page'
+                . ' or by clicking ',0,1);
+        $pdf->SetTextColor(0,0,255);
+        $pdf->SetFont('','U');
+        $pdf->Write(10,'here','http://wal-engproject.rhcloud.com/src/pay_bill.php?id=' . $_GET['id']);
+        $pdf->SetTextColor(0,0,0);
         $pdf->Cell(50,20,'',0,1);
         $pdf->SetFont('Arial','B',16);
         $pdf->Cell($pdf->w-20,10,'Billing Details:',0,1,'C');
         $pdf->SetFont('Arial','',12);
-        $pdf->Cell($pdf->w-20,10,'Doctor Services: $500',0,1,'C');
-        $pdf->Cell($pdf->w-20,10,'Prescription: $200','B',1,'C');
-        $pdf->SetFont('Arial','B');
-        $pdf->Cell(50,10,'Total: $700',0,1);
+        if (!empty($prescriptionInfo)) {
+            $doctorServices = intval($this->amount_due) - intval($prescriptionInfo['price']);
+            $pdf->Cell($pdf->w-20,10,'Doctor Services: $' . $doctorServices,0,1,'C');
+            $pdf->Cell($pdf->w-20,10,'Prescription: $' . $prescriptionInfo['price'],'B',1,'C');
+            $pdf->SetFont('Arial','B');
+            $pdf->Cell($pdf->w-20,10,'Total: $' . $this->amount_due,0,1,'C');
+        } else {
+            $pdf->Cell($pdf->w-20,10,'Doctor Services: $' . $this->amount_due,'B',1,'C');
+            $pdf->SetFont('Arial','B');
+            $pdf->Cell($pdf->w-20,10,'Total: $' . $this->amount_due,0,1,'C');
+        }
         $firstLastName = explode(" ", $this->patientName);
         $fileName = $firstLastName[1] . "_Bill.pdf";
         $pdf->Output($fileName,'F');
@@ -296,29 +339,70 @@ class Diagnosis {
         $pdf = new FPDF();
         $pdf->AddPage();
         $pdf->SetFont('Arial','B',22);
-        $pdf->Cell($pdf->w,40,'Billing Information',0,1,'C');
+        $pdf->Cell($pdf->w-20,40,'Billing Receipt',0,1,'C');
         $pdf->SetFont('Arial','',12);
-        $pdf->MultiCell($pdf->w-20,10,'Thank you ' . $this->patientName . ' for scheduling and attending your appointment with ' . $this->doctorName
-                . '. The doctor had the following observations:',0,1);
-        $pdf->MultiCell(100,10,$this->observations,0,1);
+        
+        $query = '
+                SELECT *
+                FROM prescription
+                WHERE
+                    id = :id
+                  ';
+        $query_params = array(
+            ':id' => $this->prescriptionID
+        );
+        try {
+            $stmt = $db->prepare($query);
+            $stmt->execute($query_params);
+        } catch(PDOException $ex) {
+            die("Failed to run query: " . $ex->getMessage());
+        }
+        $prescriptionInfo = $stmt->fetch();
+
+        $pdf->MultiCell($pdf->w-20,10,'Thank you, ' . $this->patientName . ', for scheduling and attending your appointment with ' . $this->doctorName
+                . '. The doctor had the following observations:',0);
+        $pdf->SetFont('Arial','B');
+        $pdf->MultiCell($pdf->w-60,8,$this->observations,0,'C');
+        $pdf->SetFont('Arial','',12);
         $pdf->Write(10,'These observations led to the following diagnosis: ');
         $pdf->SetFont('Arial','B');
         $pdf->Cell(30,10,$this->diagnosis,0,1);
         $pdf->SetFont('Arial','');
-        $pdf->Write($pdf->w-20,10,'You have therefore been given this medication: ');
-        $pdf->SetFont('Arial','B');
-        $pdf->Cell(30,10,'SOME MEDICATION',0,1);
-        $pdf->SetFont('Arial','');
-        $pdf->MultiCell($pdf->w-20,10,'Please submit you payment soon by clicking on the Pay Bills link on the home page of your'
-                . ' account or by clicking HERE.',0,1);
+        if (!empty($prescriptionInfo)) {
+            $pdf->Write(10,'You have therefore been given this medication: ');
+            $pdf->SetFont('Arial','B');
+            $pdf->Cell(30,10,$this->medication,0,1);
+            $pdf->SetFont('Arial','');
+            $pdf->Write(10,'General information: ');
+            $pdf->SetFont('Arial','B');
+            $pdf->MultiCell(60,8,$prescriptionInfo['property'],0);
+            $pdf->SetFont('Arial','');
+            $pdf->Write(10,'Directions of usage: ');
+            $pdf->SetFont('Arial','B');
+            $pdf->MultiCell(60,8,$prescriptionInfo['usage_directions'],0);
+            $pdf->SetFont('Arial','');
+        }
+        $pdf->Write(10,'Please submit you payment soon by clicking on the Pay link next to your bill on the view bills page'
+                . ' or by clicking ',0,1);
+        $pdf->SetTextColor(0,0,255);
+        $pdf->SetFont('','U');
+        $pdf->Write(10,'here','http://wal-engproject.rhcloud.com/src/pay_bill.php?id=' . $_GET['id']);
+        $pdf->SetTextColor(0,0,0);
         $pdf->Cell(50,20,'',0,1);
         $pdf->SetFont('Arial','B',16);
         $pdf->Cell($pdf->w-20,10,'Billing Details:',0,1,'C');
         $pdf->SetFont('Arial','',12);
-        $pdf->Cell($pdf->w-20,10,'Doctor Services: $500',0,1,'C');
-        $pdf->Cell($pdf->w-20,10,'Prescription: $200','B',1,'C');
-        $pdf->SetFont('Arial','B');
-        $pdf->Cell(50,10,'Total: $700',0,1);
+        if (!empty($prescriptionInfo)) {
+            $doctorServices = intval($this->amount_due) - intval($prescriptionInfo['price']);
+            $pdf->Cell($pdf->w-20,10,'Doctor Services: $' . $doctorServices,0,1,'C');
+            $pdf->Cell($pdf->w-20,10,'Prescription: $' . $prescriptionInfo['price'],'B',1,'C');
+            $pdf->SetFont('Arial','B');
+            $pdf->Cell($pdf->w-20,10,'Total: $' . $this->amount_due,0,1,'C');
+        } else {
+            $pdf->Cell($pdf->w-20,10,'Doctor Services: $' . $this->amount_due,'B',1,'C');
+            $pdf->SetFont('Arial','B');
+            $pdf->Cell($pdf->w-20,10,'Total: $' . $this->amount_due,0,1,'C');
+        }
         $firstLastName = explode(" ", $this->patientName);
         $fileName = $firstLastName[1] . "_Bill.pdf";
         $pdf->Output($fileName,'F');
@@ -360,7 +444,7 @@ class Diagnosis {
             $stmt = $this->db->prepare($query);
             $stmt->execute($query_params);
         } catch(PDOException $e) {
-            die("Failed to delete appointment. " . $e->getMessage());
+            die("Failed to update appointment information. " . $e->getMessage());
         }
     }
 }
