6bd6d048a77c587ba95a389e3aa45e024f9f2b81
Anu Ghimire <anughimire@gmail.com>
2015-04-17 10:50:10 -0500

    Logo location

--- src/bill_receipt.php
+++ src/bill_receipt.php
@@ -10,7 +10,8 @@ $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',22);
 $pdf->Cell($pdf->w-20,40,'Billing Receipt',0,1,'C');
-$pdf->Cell(40, 40, $pdf->Image($logo, $pdf->GetX(), $pdf->GetY(), 33.78),0,0,'L', false);
+$pdf->Cell(0, 0, $pdf->Image($logo, 1,1,33.78), 0, 0, 'C', false,'');
+//$pdf->Image($logo, 5, 70, 33.78);
 
 $pdf->SetFont('Arial','',12);
 
