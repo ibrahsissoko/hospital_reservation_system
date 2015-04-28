ab4077105473bdd1c1832d3bcf12e5b3ed7248c3
Luke Klinker <lklinker1@gmail.com>
2015-04-28 11:05:36 -0500

    confirmation for admins releasing bills

--- src/release_checks.php
+++ src/release_checks.php
@@ -87,13 +87,23 @@ if(empty($_SESSION['user'])) {
             echo '<tr><td>' . $doctorInfo['first_name'] . ' ' . $doctorInfo['last_name'] .'</td><td>' . $row['date'] . '</td><td>$' . $row['amount_due'] . '</td>';
             $link1 = "http://wal-engproject.rhcloud.com/src/check_receipt.php?id=" . $row['doctor_id'];
             $link2 = "http://wal-engproject.rhcloud.com/src/release_check_executor.php?id=" . $row['id'];
-            echo '<td><a href="' . $link1 . '">Receipt</a></td><td><a href="' . $link2 . '">Release</a></td></tr>';
+            echo '<td><a href="' . $link1 . '">Receipt</a></td><td><a href="' . $link2 . '" class="confirmation">Release</a></td></tr>';
         }
         echo '</table><br/><br/>';
     } else {
         echo "You have no checks to view right now.";
     }
     ?>
+
+    <script type="text/javascript">
+        var elems = document.getElementsByClassName('confirmation');
+        var confirmIt = function (e) {
+            if (!confirm('Are you sure?')) e.preventDefault();
+        };
+        for (var i = 0, l = elems.length; i < l; i++) {
+            elems[i].addEventListener('click', confirmIt, false);
+        }
+    </script>
 </div>
 
 </body>
