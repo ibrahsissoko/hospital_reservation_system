f963e6f8dfb45eba69408eba924b6e96bc00cc72
Luke Klinker <lklinker1@gmail.com>
2015-04-28 10:34:48 -0500

    insert java script

--- src/release_bill_executor.php
+++ src/release_bill_executor.php
@@ -102,7 +102,17 @@ require("fpdf17/fpdf.php");
 
     ?>
 
-    <a href="release_bills.php" onsubmit="return confirm('testing')" >testing</a>
+    <a href="release_bills.php" class="confirmation" >testing</a>
+
+    <script type="text/javascript">
+        var elems = document.getElementsByClassName('confirmation');
+        var confirmIt = function (e) {
+            if (!confirm('Are you sure?')) e.preventDefault();
+        };
+        for (var i = 0, l = elems.length; i < l; i++) {
+            elems[i].addEventListener('click', confirmIt, false);
+        }
+    </script>
 </div>
 
 </body>
