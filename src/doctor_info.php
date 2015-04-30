d1fde04acd9a7cf5bacc85fdb7e06277512e3088
Luke Klinker <lklinker1@gmail.com>
2015-04-30 10:10:58 -0500

    try again

--- src/doctor_info.php
+++ src/doctor_info.php
@@ -100,7 +100,7 @@
             }
 
             $row = $stmt->fetch();
-            $user = $row;
+            $availability = $row['availability'];
             $departmentId = $row['department_id'];
 
             $query = "
@@ -132,11 +132,11 @@
         <input type="text" name="years_of_experience" value="<?php echo htmlspecialchars($_SESSION['user']['years_of_experience']);?>" />
         <br/>
         Availability:<br/>
-        <input type="checkbox" name="availability[]" value="M" <?php (strpos($user['availability'],'M') !== false) ? 'checked=checked' : '' ?> /> Monday<br/>
-        <input type="checkbox" name="availability[]" value="T" <?php (strpos($user['availability'],'T') !== false) ? 'checked=checked' : '' ?> /> Tuesday<br/>
-        <input type="checkbox" name="availability[]" value="W" <?php (strpos($user['availability'],'W') !== false) ? 'checked=checked' : '' ?> /> Wednesday<br/>
-        <input type="checkbox" name="availability[]" value="R" <?php (strpos($user['availability'],'R') !== false) ? 'checked=checked' : '' ?> /> Thursday<br/>
-        <input type="checkbox" name="availability[]" value="F" <?php (strpos($user['availability'],'F') !== false) ? 'checked=checked' : '' ?> /> Friday<br/><br/>
+        <input type="checkbox" name="availability[]" value="M" <?php (strpos($availability,'M') !== false) ? 'checked=checked' : '' ?> /> Monday<br/>
+        <input type="checkbox" name="availability[]" value="T" <?php (strpos($availability,'T') !== false) ? 'checked=checked' : '' ?> /> Tuesday<br/>
+        <input type="checkbox" name="availability[]" value="W" <?php (strpos($availability,'W') !== false) ? 'checked=checked' : '' ?> /> Wednesday<br/>
+        <input type="checkbox" name="availability[]" value="R" <?php (strpos($availability,'R') !== false) ? 'checked=checked' : '' ?> /> Thursday<br/>
+        <input type="checkbox" name="availability[]" value="F" <?php (strpos($availability,'F') !== false) ? 'checked=checked' : '' ?> /> Friday<br/><br/>
         Shift:
         <select name="shift_id">
             <?php
